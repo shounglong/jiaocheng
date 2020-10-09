d160f0afde47c34482b39a1cf3c15327f6087091
nanyi <2752467928@qq.com>
2020-10-09 14:25:28 +0800

    恢复

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">123456</router-link> |
+      <router-link to="/">home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
